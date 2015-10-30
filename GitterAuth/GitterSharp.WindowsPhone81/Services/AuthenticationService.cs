a55f291763e1b6729f3126f5273fb1971c89e948
Odonno <bottiau.david@laposte.net>
2015-10-30 14:14:10 +0100

    refactor(app): add linked files in both W8.1 and WP8.1 projects

--- GitterAuth/GitterSharp.WindowsPhone81/Services/AuthenticationService.cs
+++ GitterAuth/GitterSharp.WindowsPhone81/Services/AuthenticationService.cs
@@ -1,20 +1,28 @@
 ﻿using GitterSharp.Helpers;
 using System;
 using System.Threading.Tasks;
-using Windows.ApplicationModel.Activation;
 using Windows.Security.Authentication.Web;
+#if WINDOWS_PHONE_APP
+using Windows.ApplicationModel.Activation;
+#endif
 
 namespace GitterSharp.Services
 {
     /// <summary>
-    /// Service used to finalize the authentication using Web Authentication Broker on Windows Phone 8.1
+    /// Service used to finalize the authentication using Web Authentication Broker on Windows (Phone) 8.1
     /// </summary>
     public class AuthenticationService
     {
         #region Fields
 
+#if WINDOWS_PHONE_APP
         private static string _oauthKey { get; set; }
         private static string _oauthSecret { get; set; }
+#endif
+
+#if WINDOWS_APP
+        private static string _token { get; set; }
+#endif
 
         #endregion
 
@@ -30,22 +38,32 @@ namespace GitterSharp.Services
         {
             try
             {
+#if WINDOWS_PHONE_APP
                 _oauthKey = oauthKey;
                 _oauthSecret = oauthSecret;
+#endif
 
                 string startUrl = $"https://gitter.im/login/oauth/authorize?client_id={oauthKey}&response_type=code&redirect_uri={AuthHelper.RedirectUrl}";
                 var startUri = new Uri(startUrl);
                 var endUri = new Uri(AuthHelper.RedirectUrl);
 
+#if WINDOWS_PHONE_APP
                 WebAuthenticationBroker.AuthenticateAndContinue(startUri, endUri, null, WebAuthenticationOptions.None);
-
                 return await Task.FromResult<bool?>(null);
+#endif
+#if WINDOWS_APP
+                var webAuthenticationResult = await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None, startUri, endUri);
+                _token = await AuthHelper.RetrieveToken(webAuthenticationResult, oauthKey, oauthSecret);
+                return !string.IsNullOrWhiteSpace(_token);
+#endif
             }
             catch
             {
                 return null;
             }
         }
+
+#if WINDOWS_PHONE_APP
 
         /// <summary>
         /// Retrieve token to use Gitter Api methods that requires a connected user
@@ -69,6 +87,21 @@ namespace GitterSharp.Services
 
             return null;
         }
+
+#endif
+
+#if WINDOWS_APP
+
+        /// <summary>
+        /// Retrieve token to use Gitter Api methods that requires a connected user
+        /// </summary>
+        /// <returns>The token</returns>
+        public Task<string> RetrieveTokenAsync()
+        {
+            return Task.FromResult(_token);
+        }
+
+#endif
 
         #endregion
     }
